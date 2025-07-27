d98f63664fd3b60ba2a5a5255135e8dac071ef7d
Pedro H G Migacz <pedromigacz@gmail.com>
2025-07-26 21:06:58 -0300

    Add prisma skip to check if it's at fault

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,2 +1,3 @@
 export { db } from "./client";
+export { Prisma } from "./generated/client/client";
 export * from "./generated/client";
--- packages/web/app/posts-from-server-action/getPosts.ts
+++ packages/web/app/posts-from-server-action/getPosts.ts
@@ -1,12 +1,13 @@
 "use server";
 
 import { db } from "db";
-import { POST_TYPE } from "db"
+import { POST_TYPE, Prisma } from "db"
 
 export async function getPosts() {
   const posts = await db.post.findMany()
 
   console.log("POST_TYPE", POST_TYPE)
+  console.log("Prisma.skip", Prisma.skip)
 
   return { posts };
 }
